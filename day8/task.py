e6f8448a090404123b630c4024057ca607ca4078
KaroGil <karolinkagil@gmail.com>
2023-12-09 11:59:59 +0100

    day 8 part 2

--- day8/task.py
+++ day8/task.py
@@ -1,3 +1,5 @@
+import math
+
 def checkEndPoint(choosen, end):
     if choosen == end:
         return True
@@ -22,37 +24,60 @@ with open("input.txt") as f:
     _, end = mappings[-1].split(" = ")[1].split(" ")
     end = end.strip("()")
     end = "ZZZ"
-    print("end", end)
-
-    print(mappings) 
-
     first = mappings[0].split(" = ")[0]
 
     mapped = {}
 
+    allA = []
+    allZ = []
+
     for i in range(len(mappings)):
         elem = mappings[i]
         fromElem, toElem = elem.split(" = ")
-        print(fromElem, toElem)
+        #print(fromElem, toElem)
         (a,b) = toElem.split(" ")
         toElem = (a.strip("()"),b.strip("()"))
 
+        if "A" in fromElem:
+            allA.append(fromElem)
+        if "Z" in toElem:
+            allZ.append(fromElem)
+
         mapped[fromElem] = toElem
 
-    print(mapped)
+    #print(mapped)
 
-    start = "AAA"
-    i = 0
-    while True:
-        way = direction[i%len(direction)]
-        toElem = mapped[start]
-        choosen = toElem[checkDirection(way)]
-        step_count+=1
-        i+=1
-        choosen = choosen.strip(",")
-        start = choosen
-        print(step_count)
-        if(checkEndPoint(choosen, end)):
-            break
+    #part 1
+    # start = "AAA"
+    # i = 0
+    # while True:
+    #     way = direction[i%len(direction)]
+    #     toElem = mapped[start]
+    #     choosen = toElem[checkDirection(way)]
+    #     step_count+=1
+    #     i+=1
+    #     choosen = choosen.strip(",")
+    #     start = choosen
+    #     print(step_count)
+    #     if(checkEndPoint(choosen, end)):
+    #         break
 
-print(step_count)
+#print(step_count)
+
+
+#part 2
+    def getSteps(startNode):
+        count = 0
+        while startNode[2] != "Z":
+            way = direction[count%len(direction)]
+            toElem = mapped[startNode]
+            choosen = toElem[checkDirection(way)]
+            count+=1
+            startNode = choosen.strip(",")
+        return count 
+        
+lengths = [getSteps(x) for x in allA]
+print(lengths)
+
+ans = math.lcm(*lengths)
+print(ans)
